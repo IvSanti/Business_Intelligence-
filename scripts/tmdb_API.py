51df62d24736f2b5c1dda3dcf33af3bd0dc191ae
santiago <iscastanedah@uqvirtual.edu.co>
2020-11-23 20:45:46 -0500

    conexion y peticion a la api

--- scripts/tmdb_API.py
+++ scripts/tmdb_API.py
@@ -1,8 +1,34 @@
 import requests
+import json
+import os
+
 
 path = 'https://api.themoviedb.org/3'
 token = requests.get(path + '/authentication/token/new')
-apikey = ''
+apikey = '8554f4977a50ffae690ef3874fb5c5e6'
 
-response = requests.get(path + '/discover/movie')
-print(response)
+dir = 'archivo_json'
+file_name = "data.json"
+
+response = requests.get(path + '/discover/movie?sort_by=popularity.desc?page=1'
+                        + '&api_key='+apikey+'&language=es&include_image_lenguage=es')
+
+print('RESPONSE:' + response.text)
+
+data = json.loads(response.text)
+pages = data['total_pages']
+
+print(json.loads(response.text)['results'])
+
+for i in range(pages):
+
+   response = requests.get(path + '/discover/movie?sort_by=popularity.desc?page='+(i+1)
+                           + '&api_key='+apikey+'&language=es&include_image_lenguage=es')
+   data.append(json.loads(response.text)['results'])
+
+with open(os.path.join(dir, file_name), 'w') as file:
+    json.dump(data, file)
+
+print(data['results'])
+
+print(path + '/discover/movie?sort_by=popularity.desc?page=1'+ '&api_key='+apikey+'&language=es&include_image_lenguage=es')
