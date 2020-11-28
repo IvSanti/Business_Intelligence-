95bcea4ab846bfdbc384bf8decc217b3ffbbd392
Jhonnatan <jhonnaerun@gmail.com>
2020-11-27 21:58:36 -0500

    implementacion consulta de detalle movie, creditos movie

--- scripts/tmdb_API.py
+++ scripts/tmdb_API.py
@@ -6,29 +6,34 @@ import os
 path = 'https://api.themoviedb.org/3'
 token = requests.get(path + '/authentication/token/new')
 apikey = '8554f4977a50ffae690ef3874fb5c5e6'
+filterPath = '&language=es&include_image_lenguage=es'
 
 dir = 'archivo_json'
-file_name = "data.json"
+file_name1 = "dataMovies.json"
+file_name2 = "dataMovieDetail.json"
+file_name3 = "dataMovieCredits.json"
 
-response = requests.get(path + '/discover/movie?sort_by=popularity.desc?page=1'
-                        + '&api_key='+apikey+'&language=es&include_image_lenguage=es')
 
-print('RESPONSE:' + response.text)
+responseMovie = requests.get(path + '/discover/movie?sort_by=popularity.desc?page=1'
+                        + '&api_key='+apikey+filterPath)
+dataMovie = json.loads(responseMovie.text)
+movies = dataMovie['total_pages']
+print('RESPONSE MOVIES:' + responseMovie)
 
-data = json.loads(response.text)
-pages = data['total_pages']
+with open(os.path.join(dir, file_name1), 'w') as file:
+    json.dump(dataMovie, file)
 
-print(json.loads(response.text)['results'])
+movieId = movies[0].id
+print('MOVIE ID:' + movieId)
 
-for i in range(pages):
+responseDetail = requests.get(path + '/movie/'+movieId+'?a=1'+ '&api_key='+apikey+filterPath)
+dataDetail = json.loads(responseDetail)
+print('RESPONSE DETAIL MOVIE:' + responseDetail)
+with open(os.path.join(dir, file_name2), 'w') as file:
+    json.dump(dataDetail, file)
 
-   response = requests.get(path + '/discover/movie?sort_by=popularity.desc?page='+(i+1)
-                           + '&api_key='+apikey+'&language=es&include_image_lenguage=es')
-   data.append(json.loads(response.text)['results'])
-
-with open(os.path.join(dir, file_name), 'w') as file:
-    json.dump(data, file)
-
-print(data['results'])
-
-print(path + '/discover/movie?sort_by=popularity.desc?page=1'+ '&api_key='+apikey+'&language=es&include_image_lenguage=es')
+responseCredits = requests.get(path + '/movie/'+movieId+'/credits?a=1'+ '&api_key='+apikey+filterPath)
+dataCredits = json.loads(responseCredits)
+print('RESPONSE CREDITS:' + responseCredits)
+with open(os.path.join(dir, file_name2), 'w') as file:
+    json.dump(dataCredits, file)
