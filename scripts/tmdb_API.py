94da7db91a199d5b68810e26c84959fd7cd48312
santiago <iscastanedah@uqvirtual.edu.co>
2020-11-29 17:23:37 -0500

    corrección de bugs

--- scripts/tmdb_API.py
+++ scripts/tmdb_API.py
@@ -9,39 +9,45 @@ token = requests.get(path + '/authentication/token/new')
 apikey = '8554f4977a50ffae690ef3874fb5c5e6'
 filterPath = '&language=es&include_image_lenguage=es'
 
-dir = './archivo_json/'
+dir = '../archivo_json'
 file_name1 = "dataMovies.json"
 file_name2 = "dataMovieDetail.json"
 file_name3 = "dataMovieCredits.json"
 
 
-responseMovie = requests.get(path + '/discover/movie?sort_by=popularity.desc?page=1'
+print(path + '/discover/movie?primary_release_date.gte=2020-01-01&primary_release_date.lte=2020-10-01'
                         + '&api_key='+apikey+filterPath)
-dataMovie = json.loads(responseMovie.text)
-movies = dataMovie['total_pages']
-print('RESPONSE MOVIES:' + responseMovie)
 
+
+responseMovie = requests.get(path + '/discover/movie?primary_release_date.gte=2020-01-01&primary_release_date.lte=2020-10-01'
+                        + '&api_key='+apikey+filterPath)
+
+print(responseMovie.text)
+
+dataMovie = json.loads(responseMovie.text)
+
+movies = dataMovie['results']
+print(dataMovie)
 with open(os.path.join(dir, file_name1), 'w') as file:
     json.dump(dataMovie, file)
 
-movieId = movies[0].id
-print('MOVIE ID:' + movieId)
+movieId = dataMovie['results'][0]['id']
+print(movieId)
 
-responseDetail = requests.get(path + '/movie/'+movieId+'?a=1'+ '&api_key='+apikey+filterPath)
-dataDetail = json.loads(responseDetail)
-print('RESPONSE DETAIL MOVIE:' + responseDetail)
+responseDetail = requests.get(path + '/movie/'+str(movieId)+'?a=1'+ '&api_key='+apikey+filterPath)
+print(responseDetail)
+dataDetail = json.loads(responseDetail.text)
 with open(os.path.join(dir, file_name2), 'w') as file:
     json.dump(dataDetail, file)
 
-responseCredits = requests.get(path + '/movie/'+movieId+'/credits?a=1'+ '&api_key='+apikey+filterPath)
-dataCredits = json.loads(responseCredits)
-print('RESPONSE CREDITS:' + responseCredits)
-with open(os.path.join(dir, file_name2), 'w') as file:
+responseCredits = requests.get(path + '/movie/'+str(movieId)+'/credits?a=1'+ '&api_key='+apikey+filterPath)
+dataCredits = json.loads(responseCredits.text)
+with open(os.path.join(dir, file_name3), 'w') as file:
     json.dump(dataCredits, file)
 
 keyDate = time.strftime("%c")
-s3_client = boto3.client('s3', aws_access_key_id="AKIAJUJLNTABCW4CJT4Q",
-                      aws_secret_access_key="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")
+s3_client = boto3.client('s3', aws_access_key_id="AKIAIR4JM2V6HJN4ID6A",
+                      aws_secret_access_key="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")
 
 nombre_archivo1 = file_name1+keyDate+".json"
 nombre_archivo2 = file_name2+keyDate+".json"
