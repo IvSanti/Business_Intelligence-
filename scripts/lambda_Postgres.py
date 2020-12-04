8dbaf7be540616f0265c72a564562d05093bf396
santiago <iscastanedah@uqvirtual.edu.co>
2020-12-03 21:48:50 -0500

    lambda cargar datos a postgres

--- scripts/lambda_Postgres.py
+++ scripts/lambda_Postgres.py
@@ -1,52 +1,33 @@
+import psycopg2
 import requests
 import json
 import os
 import boto3
+import urllib
 import time
 import urllib.parse
 
-print('Loading function')
-s3 = boto3.client('s3', aws_access_key_id="AKIA6DDOJFQECCP6GSGW",
-                    aws_secret_access_key="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")
+from io import BytesIO
 
-##ss3 = boto3.resource('s3')
-##archivos = s3.list_obects(Bucket='proyecto-final-bi')['Contents']
-##for key in archivos:
-##    objeto = key['key']
-##    if not objeto.endswith("/"):
-##        print((Bucket='proyecto-final-bi', key = objeto)['Body'].read().decode('utf-8'))
-##print('SALE FOR')
-##print('SALE FOR')
-##print(archivos)
-##
+connection = psycopg2.connect(user="postgres", password="postgres",
+                              host="datos-tmdb.cbgtaxmzmelf.us-east-2.rds.amazonaws.com",
+                              port="5432", database="postgres")
 
+fecha = time.strftime("%c")
 
-#def lambda_handler(event):
-#, aws_access_key_id="AKIA6DDOJFQECCP6GSGW",
- # aws_secret_access_key="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"
+cursor = connection.cursor()
+datos = ["1","3",fecha,"3" ,"3","3","3","3",fecha,fecha]
+datos_query = ""
+for i in datos:
+    datos_query +=  "'"+str(i)+"',"
 
-bucket = 'sustentacion'
-directorio = 'Endpoints/'
-
-key1 = directorio + 'dataMovieDetail.jsonMon Nov 30 19:57:44 2020.json'
-key2 = directorio + 'dataMovieCredits.jsonWed Dec 2 23:55:01 2020.json'
-key3 = directorio + 'dataMovies.jsonMon Nov 30 19:57:44 2020.json'
-
-print(key1)
-
-try:
-    response1 = s3.get_object(Bucket=bucket, Key=key1)
-    response2 = s3.get_object(Bucket=bucket, Key=key1)
-    response3 = s3.get_object(Bucket=bucket, Key=key1)
-
-    contenido = response1['Body'].read().decode('utf-8')
-    print (contenido)
-
-    # print("CONTENT TYPE: " + response1['ContentType'])
-    # return response['ContentType']
-except Exception as e:
-    print(e)
-    print('Error getting object {} '
-          'from bucket {}. '
-          'Make sure they exist and your bucket is in the same region as this function.'.format(key1, bucket))
-    raise e
+datos_query = datos_query[:len(datos_query)-1]
+print(datos_query)
+postgres_insert_query = "INSERT INTO pelicula"+\
+                                "(id_pelicula,nombre_pelicula,fecha_lanzamiento,popularidad,sinopsis,origen,api,cloudWatch,fecha_consulta,fecha_carga) "+\
+                        "VALUES ("+ datos_query +")"
+cursor.execute(postgres_insert_query)
+connection.commit()
+count=cursor.rowcount
+print(postgres_insert_query)
+print(count, "Record inserted successfully")
